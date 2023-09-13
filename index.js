d19eda9bc61faa7a2d59ded129167f5165dd88d0
wt <wat0x01@sp.noahgroup.com>
2023-09-13 23:28:34 +0800

    add icon js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import Button from "./src/button"
 import ButtonGroup from "./src/button-group"
 import Icon from "./src/icon"
 import "./index.scss"
-import "https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_16316_7.d88121c5474511d5b374844b98e75c04.js"
+import "./icon.js"
 export { Button, ButtonGroup, Icon }
