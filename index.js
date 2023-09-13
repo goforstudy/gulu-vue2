d2b875f7515f180dbd0d7e39d298baeda50c6964
wt <wat0x01@sp.noahgroup.com>
2023-09-13 23:17:43 +0800

    modify style

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Button from "./src/button"
 import ButtonGroup from "./src/button-group"
 import Icon from "./src/icon"
-
+import "./index.scss"
 export { Button, ButtonGroup, Icon }
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import GButton from "./src/button.vue"
 import GIcon from "./src/icon.vue"
 import GButtonGroup from "./src/button-group.vue"
+import "./index.scss"
 Vue.component('g-button', GButton)
 Vue.component('g-icon', GIcon)
 Vue.component('g-button-group', GButtonGroup)
