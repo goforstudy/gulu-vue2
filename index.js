ef8fd9068b3f6b414dd1425f923fd3d584cbd798
wt <wat0x01@sp.noahgroup.com>
2023-09-26 20:12:54 +0800

    add toast components

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import "./index.scss";
 import "./icon.js";
 import injectAuto from "./util/injectComponent"
+import toast from "./src/plugin.js"
 const components = injectAuto.readComponents();
 const install = function (Vue, opts = {}) {
   components.forEach((component) => {
     Vue.component(component.name, component);
   });
+  toast.install(Vue)
 };
 export default { install };
